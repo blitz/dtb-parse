7534e4b6a1b057d4ca4c9b99ce671d3ee1d049fa
Julian Stecklina <js@alien8.de>
2020-11-06 17:12:38 +0200

    Add ULX3s SaxonSoc DTB as test

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 module ParseSpec (spec) where
 
+import qualified Data.ByteString as B
+import           Data.FileEmbed  (embedFile)
 import           Test.Hspec
+
+-- |A flattened device tree describing the ULX3S running SaxonSoc.
+--
+-- See https://github.com/lawrie/saxonsoc-ulx3s-bin
+dtbFile :: B.ByteString
+dtbFile = $(embedFile "test/ulx3s-green85f.dtb")
 
 spec :: Spec
 spec = describe "parser" $ do
